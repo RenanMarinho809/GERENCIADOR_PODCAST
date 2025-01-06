7ad0059bc2d3edafd882df602d9592c90218f3a5
RenanMarinho809 <renanmarinho809@gmail.com>
2025-01-06 18:02:43 -0300

    criando controller

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,17 @@
-console.log("hello world!");
+import * as http from 'http';
+
+import { getListEpisodes } from './controllers/podscasts-controller';
+
+const server = http.createServer( async (req: http.IncomingMessage, res: http.ServerResponse) => {
+     
+    if (req.method === 'GET') {
+      await  getListEpisodes(req, res);
+    }
+
+});
+
+const port = process.env.PORT 
+
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
