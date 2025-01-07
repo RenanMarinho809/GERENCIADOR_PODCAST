1055d11cf5e4dfef6fb44a41eb258e71dc3761d8
RenanMarinho809 <renanmarinho809@gmail.com>
2025-01-07 17:46:49 -0300

    Implement podcast management features with episode listing and repository integration

--- src/controllers/podscasts-controller.ts
+++ src/controllers/podscasts-controller.ts
@@ -1,9 +1,14 @@
 import { IncomingMessage, ServerResponse} from 'http'
 
+import { serviceListEpisodes } from '../services/list-episodes-service';
+
+
+
 
 export const getListEpisodes = async(req: IncomingMessage, res: ServerResponse) => {
+ 
+      const content = await serviceListEpisodes(); 
+        
        res.writeHead(200, {'content-type' : 'application/json'});
-         res.end(({
-            name: 'felipe'  
-         }));
+         res.end(JSON.stringify(content));  
 };
