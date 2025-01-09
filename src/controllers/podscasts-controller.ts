40b963cc293b1ae8d351dbd07af6ce9fd2923287
RenanMarinho809 <renanmarinho809@gmail.com>
2025-01-09 14:44:16 -0300

    Refactor episode services to improve response formatting and type safety

--- src/controllers/podscasts-controller.ts
+++ src/controllers/podscasts-controller.ts
@@ -5,13 +5,14 @@ import { serviceFilterEpisodes } from '../services/filter-episodes-service';
 import { StatusCode } from '../utils/status-code';
 import { contentType } from '../utils/content-type';
 import { FilterPodcastModel } from '../models/filter-podcast-model';
+import { PodcastModel } from '../models/podcast-model';
 
 
 
 
 export const getListEpisodes = async(req: IncomingMessage, res: ServerResponse) => {
  
-      const content = await serviceListEpisodes(); 
+      const content : PodcastModel[] = await serviceListEpisodes(); 
         
        res.writeHead(StatusCode.OK, {'content-type' : contentType.JSON});
          res.end(JSON.stringify(content));  
--- src/services/filter-episodes-service.ts
+++ src/services/filter-episodes-service.ts
@@ -4,6 +4,7 @@ import { StatusCode } from "../utils/status-code";
 
 
 
+
 export const serviceFilterEpisodes = async (podcastName: string | undefined): Promise<FilterPodcastModel> => {
 
     let responseFormat : FilterPodcastModel = {
@@ -12,15 +13,17 @@ export const serviceFilterEpisodes = async (podcastName: string | undefined): Pr
 
     };
 
+
     const queryString = podcastName?.split("?p=")[1] ?? "";
+    
 
     const data = await repositoryPodcast(queryString);
 
-    responseFormat.statusCode = data.length !== 0 ? StatusCode.OK : StatusCode.NoContent
 
-   
-
-    responseFormat.body = data;
-
+    responseFormat = {
+      statusCode: data.length !== 0 ? StatusCode.OK : StatusCode.NoContent,
+      body: data,
+    };
+  
     return responseFormat;
 };
